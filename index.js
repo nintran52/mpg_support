a942e4561eb3178f757ebad7d92710711943dfc0
Nin Tran <nin.tran52@gmail.com>
2022-06-23 15:08:05 +0700

    feat(data): update model

--- index.js
+++ index.js
@@ -23,43 +23,19 @@ var options = {
 
 var client = mqtt.connect("tcp://mqtt.mypetgo.com", options);
 
-var template = {
+var templatePetHealth = {
   header: {
-    did: "deviceCode",
-    dateTime: "2022-04-13T09:11:13Z",
+    id: "deviceCode",
+    dat: "2022-04-13T09:11:13Z",
     net: "W",
   },
   data: {
     lat: 16.0742,
     lon: 108.17687,
-    altitude: 12.7,
-  },
-};
-
-var templatePetHealth = {
-  header: {
-    did: "deviceCode",
-    dateTime: "2022-04-13T09:11:13Z",
-    net: "W",
-  },
-  data: {
-    petTemp: 29.68,
-    envTemp: 28.53,
-    bat: 69,
-    motion: 0,
-    steps: 0,
-    itchScratch: 0,
-    eatDrink: 0,
-    resting: 0,
-    sleeping: 0,
-    sitting: 0,
-    standing: 0,
-    netInfo: {
-      ssid: "MyPetGo",
-      ip: "192.168.88.136",
-      mac: "cc:71:90:09:0d:ea",
-      rssi: -52,
-    },
+    alt: 5.7,
+    tmp: 38.68,
+    env: 27.53,
+    ste: 0,
   },
 };
 
@@ -67,32 +43,24 @@ var templatePetHealth = {
 // https://developers.google.com/maps/documentation/utilities/polylineutility
 client.on("connect", function () {
   var routes = polyline.decode(
-    "adeGmnmxRa@KSa@Uc@Ue@Uc@Qe@Z_@ZWZOX^Vb@Pb@P^V`@P^T`@Ll@k@Bq@BIS"
+    "_ygkFpgyv@?G@I@I@GBAB@B?@?B@B?B@D?B?B@D@B@B?B?D@D?D@D@F?D@FB?FADAFAJAFAJAF?HGAGAIAI?GAGAGAGAIAGAGAE?AE?E@G?G???G"
   );
   routes.forEach(pushMessage);
 });
 
 function pushMessage(item, index) {
-  const deviceCode = "7C:9E:BD:6E:89:44";
+  const deviceCode = "7C:9E:BD:6E:89:9C";
 
   setTimeout(() => {
     let dateTime = moment().toISOString();
 
-    template.header.did = deviceCode;
-    template.header.dateTime = dateTime;
-    template.data.lat = item[0];
-    template.data.lon = item[1];
-    template.data.altitude = randomFloat(0.4, 14.32);
-    client.publish("gps", JSON.stringify(template));
-    console.log(JSON.stringify(template));
-
-    templatePetHealth.header.did = deviceCode;
-    templatePetHealth.header.dateTime = dateTime;
-    templatePetHealth.data.petTemp = randomFloat(33.54, 40.43);
-    templatePetHealth.data.envTemp = randomFloat(27.32, 33.53);
-    templatePetHealth.data.motion = randomInteger(1, 10);
-    templatePetHealth.data.steps = randomInteger(10, 20);
-    client.publish("health", JSON.stringify(templatePetHealth));
+    templatePetHealth.header.id = deviceCode;
+    templatePetHealth.header.dat = dateTime;
+    templatePetHealth.data.lat = item[0];
+    templatePetHealth.data.lon = item[1];
+    templatePetHealth.data.alt = randomFloat(0.4, 14.32);
+    templatePetHealth.data.ste = randomInteger(10, 20);
+    client.publish("pet", JSON.stringify(templatePetHealth));
     console.log(JSON.stringify(templatePetHealth));
   }, 7000 * index);
 }
